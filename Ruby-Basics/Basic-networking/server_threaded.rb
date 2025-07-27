17b260cdfd993c8de70c65a02fcfa837b8d51525
Tofame <80583148+Tofame@users.noreply.github.com>
2025-07-28 00:36:10 +0200

    add: threaded server
    Adds server that handles each client in a new thread. It is still part of basic-networking, meaning I just tested summing up which was once a project I made for Java lectures.

---
+++ Ruby-Basics/Basic-networking/server_threaded.rb
@@ -0,0 +1,40 @@
+require 'socket'
+
+port = 9000
+server = TCPServer.new(port)
+puts "Server started on port #{port}..."
+
+sum = 0
+
+loop do
+  client = server.accept
+  Thread.new(client) do |conn|
+    puts "Client connected."
+
+    loop do
+      msg = client.gets&.chomp
+      break if msg.nil? # client disconnected
+
+      puts "Received: #{msg}"
+
+      case msg.downcase
+      when "exit"
+        client.puts "Goodbye!"
+        break
+      when "count"
+        client.puts "Current sum is #{sum}"
+      else
+        begin
+          num = Float(msg)
+          sum += num
+          client.puts "Added #{num}, new sum is #{sum}"
+        rescue ArgumentError
+          client.puts "Invalid input. Send numbers, 'count' or 'exit'."
+        end
+      end
+    end
+
+    puts "Client disconnected."
+    conn.close
+  end
+end
